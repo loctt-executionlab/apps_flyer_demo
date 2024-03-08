cad0ae95cd5e54eeaf4b0285ad4ba80bf422ef08
Loc <loc.tran@executionlab.asia>
2024-03-08 10:18:00 +0700

    handle deep link (only print result)

--- lib/notifiers/app_flyer_notifier.dart
+++ lib/notifiers/app_flyer_notifier.dart
@@ -1,13 +1,17 @@
 import 'package:appsflyer_sdk/appsflyer_sdk.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'app_flyer_notifier.g.dart';
 
-final _appsFlyerOptions = AppsFlyerOptions(afDevKey: "dev key", appId: "ID");
+final _appsFlyerOptions = AppsFlyerOptions(
+    afDevKey: dotenv.get("DEV_KEY"), appId: dotenv.get("APP_ID"));
 
 @riverpod
 class AppsFlyerNotifier extends _$AppsFlyerNotifier {
   @override
   AppsflyerSdk build() {
+    print(dotenv.get("DEV_KEY"));
+    print(dotenv.get("APP_ID"));
     var sdk = AppsflyerSdk(_appsFlyerOptions);
     sdk.initSdk(
       registerConversionDataCallback: true,
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,9 +6,8 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_deep_link/app.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_deep_link/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
